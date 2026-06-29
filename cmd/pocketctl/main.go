223a1fd6f3f0eacb170bdd7c24fe99703574d707
muwb <muwb@users.noreply.github.com>
2026-06-29 11:10:03 +0800

    chore: bump version to 0.2.24
    Gitee commits:
    - chore: bump version to 0.2.24

--- cmd/pocketctl/main.go
+++ cmd/pocketctl/main.go
@@ -37,7 +37,7 @@ import (
 	"github.com/pocketctl/pocketctl/internal/ws"
 )
 
-var version = "0.2.23"
+var version = "0.2.24"
 
 // DefaultRelayURL is the public production relay used when no --relay flag,
 // --prod config, or POCKETCTL_RELAY_URL env is provided. To target a local or
