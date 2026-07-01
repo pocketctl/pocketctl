7755a3cd438abe3ec21a6d21bde97cf36578448b
muwenbin <muwenbin@58.com>
2026-07-01 09:54:32 +0800

    chore: bump version to 0.2.34

--- cmd/pocketctl/main.go
+++ cmd/pocketctl/main.go
@@ -38,7 +38,7 @@ import (
 	"github.com/pocketctl/pocketctl/internal/ws"
 )
 
-var version = "0.2.33"
+var version = "0.2.34"
 
 // DefaultRelayURL is the public production relay used when no --relay flag,
 // --prod config, or POCKETCTL_RELAY_URL env is provided. To target a local or
