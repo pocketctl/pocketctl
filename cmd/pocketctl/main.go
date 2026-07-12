4b3ff9ea082c608a7a16e8160f35fe97efc0d9ab
muwb <muwb@users.noreply.github.com>
2026-07-12 17:08:09 +0800

    chore: bump version to 0.3.0
    Gitee commits:
    - chore: bump version to 0.3.0

--- cmd/pocketctl/main.go
+++ cmd/pocketctl/main.go
@@ -39,7 +39,7 @@ import (
 	"github.com/pocketctl/pocketctl/internal/ws"
 )
 
-var version = "0.3.0-rc8"
+var version = "0.3.0"
 
 // PR2 platform defaults for the daemon entry: daemonize + service via platform
 // interface (was direct syscall.SysProcAttr{Setsid} + internal/service).
