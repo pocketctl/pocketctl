9ba21d1d648e93dd0ab51ed1455086674934bb58
muwb <muwb@users.noreply.github.com>
2026-09-22 10:01:52 +0800

    chore: bump version to v0.4.15
    Gitee commits:
    - chore: bump version to v0.4.15

--- cmd/pocketctl/main.go
+++ cmd/pocketctl/main.go
@@ -52,7 +52,7 @@ import (
 	"github.com/pocketctl/pocketctl/internal/zcode"
 )
 
-var version = "0.4.13"
+var version = "0.4.15"
 
 // PR2 platform defaults for the daemon entry: daemonize + service via platform
 // interface (was direct syscall.SysProcAttr{Setsid} + internal/service).
